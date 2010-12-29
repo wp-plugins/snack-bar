587fd6281b3e111b606087ef51530254256fb672
wpmuguru <wpmuguru@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-29 21:02:11 +0000

    add some handy links for network admins
    git-svn-id: https://plugins.svn.wordpress.org/snack-bar/trunk@327009 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- snack-bar.php
+++ snack-bar.php
@@ -17,7 +17,56 @@ function snack_bar_menu() {
 	$class = 'snack-bar';
 
 	/* Add the main siteadmin menu item */
-	$wp_admin_bar->add_menu( array( 'id' => 'snack', 'title' => __('Snacks'), 'href' => '', 'meta' => array( 'class' => $class ) ) );
+	$site_parent = $parent = 'snack';
+	$wp_admin_bar->add_menu( array( 'id' => $parent, 'title' => __('Snacks'), 'href' => '', 'meta' => array( 'class' => $class ) ) );
+
+
+	if( is_multisite() ) {
+		/* add levels for site/network sub menus */
+		$site_parent = 'snack_site';
+		$net_parent = 'snack_network';
+		$wp_admin_bar->add_menu( array( 'id' => $site_parent, 'parent' => $parent, 'title' => __('Site'), 'href' => '', 'meta' => array( 'class' => $class ) ) );
+		$wp_admin_bar->add_menu( array( 'id' => $net_parent, 'parent' => $parent, 'title' => __('Network'), 'href' => '', 'meta' => array( 'class' => $class ) ) );
+		/* add network menu items */
+		$wp_admin_bar->add_menu( array( 'id' => 'net_plugins', 'parent' => $net_parent, 'title' => __('Plugins'), 'href' => network_admin_url( 'plugins.php' ), 'meta' => array( 'class' => $class ) ) );
+		$wp_admin_bar->add_menu( array( 'id' => 'net_settings', 'parent' => $net_parent, 'title' => __('Settings'), 'href' => network_admin_url( 'settings.php' ), 'meta' => array( 'class' => $class ) ) );
+		$wp_admin_bar->add_menu( array( 'id' => 'net_themes', 'parent' => $net_parent, 'title' => __('Themes'), 'href' => network_admin_url( 'themes.php' ), 'meta' => array( 'class' => $class ) ) );
+		$wp_admin_bar->add_menu( array( 'id' => 'net_users', 'parent' => $net_parent, 'title' => __('Users'), 'href' => network_admin_url( 'users.php' ), 'meta' => array( 'class' => $class ) ) );
+	}
+
+	/* add some extra admin items */
+	$wp_admin_bar->add_menu( array( 'id' => 'site_plugins', 'parent' => $site_parent, 'title' => __('Plugins'), 'href' => admin_url( 'plugins.php' ), 'meta' => array( 'class' => $class ) ) );
+	$wp_admin_bar->add_menu( array( 'id' => 'site_themes', 'parent' => $site_parent, 'title' => __('Themes'), 'href' => admin_url( 'themes.php' ), 'meta' => array( 'class' => $class ) ) );
+	$wp_admin_bar->add_menu( array( 'id' => 'site_users', 'parent' => $site_parent, 'title' => __('Users'), 'href' => admin_url( 'users.php' ), 'meta' => array( 'class' => $class ) ) );
+
+	/* some more site actions for network admins */
+	if( is_multisite() ) {
+		$wp_admin_bar->add_menu( array( 'id' => 'site_edit', 'parent' => $site_parent, 'title' => __('Edit'), 'href' => network_admin_url( 'site-info.php?id=' . $Wpdb->blogid ), 'meta' => array( 'class' => $class ) ) );
+		if( !is_main_site() ) {
+			$items = array();
+			$blogname = get_option( 'blogname' );
+			if ( get_blog_status( $wpdb->blogid, 'deleted' ) == '1' )
+				$items['activateblog'] = array( 'message' => sprintf( __( 'You are about to activate the site %s' ), $blogname ), 'title' => __( 'Activate' ) );
+			else
+				$items['deactivateblog'] = array( 'message' => sprintf( __( 'You are about to deactivate the site %s' ), $blogname ), 'title' => __( 'Deactivate' ) );
+
+			if ( get_blog_status( $wpdb->blogid, 'archived' ) == '1' )
+				$items['unarchiveblog'] = array( 'message' => sprintf( __( 'You are about to unarchive the site %s' ), $blogname ), 'title' => __( 'Unarchive' ) );
+			else
+				$items['archiveblog'] = array( 'message' => sprintf( __( 'You are about to archive the site %s' ), $blogname ), 'title' => __( 'Archive' ) );
+
+			if ( get_blog_status( $wpdb->blogid, 'spam' ) == '1' )
+				$items['unspamblog'] = array( 'message' => sprintf( __( 'You are about to unspam the site %s' ), $blogname ), 'title' => __( 'Not Spam' ) );
+			else
+				$items['spamblog'] = array( 'message' => sprintf( __( 'You are about to mark the site %s as spam.' ), $blogname ), 'title' => __( 'Spam' ) );
+
+			if ( current_user_can( 'delete_site', $wpdb->blogid ) )
+				$items['deleteblog'] = array( 'message' => sprintf( __( 'You are about to delete the site %s' ), $blogname ), 'title' => __( 'Delete' ) );
+
+			foreach( $items as $action2 => $strings )
+				$wp_admin_bar->add_menu( array( 'id' => 'site_' . $action2, 'parent' => $site_parent, 'title' => $strings['title'], 'href' => esc_url( wp_nonce_url( network_admin_url( 'edit.php?action=confirm&amp;action2=' . $action2 . '&amp;id=' . $wpdb->blogid . '&amp;msg=' . urlencode( $strings['message'] ) ), 'confirm' ) ), 'meta' => array( 'class' => $class ) ) );
+		}
+	}
 }
 add_action( 'admin_bar_menu', 'snack_bar_menu', 1000 );
 
