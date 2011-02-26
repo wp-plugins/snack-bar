e6c1baea89d0b069e7efc4e8fe064758214d6c47
westi <westi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-26 11:10:29 +0000

    Prep for the v0.1 release
    git-svn-id: https://plugins.svn.wordpress.org/snack-bar/trunk@351730 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- snack-bar.php
+++ snack-bar.php
@@ -3,9 +3,8 @@
  Plugin Name: Snack Bar
  Plugin URI: http://wordpress.org/extend/plugins/snack-bar/
  Description: Adds a snack menu to the admin bar 
- Author: wordpressdotorg
- Version: 0.01-alpha-do-not-use-yet
- Author URI: http://wordpress.org/
+ Author: wpmuguru, westi, PeteMall
+ Version: 0.1
  */
 
 function snack_bar_menu() {
